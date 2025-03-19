aee2b73c61745b630fb85ee751cfd61bc3f07839
jhonatan2000002 <111360621+jhonatan2000002@users.noreply.github.com>
2025-03-19 09:21:05 -0300

    first commit

---
+++ src/main/java/com/jhon/controlecontas/frontend/ControleContasFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.jhon.controlecontas.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleContasFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleContasFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhon/controlecontas/frontend/ControleContasFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhon.controlecontas.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleContasFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
